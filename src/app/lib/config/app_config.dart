149e8ef9a79dc3f279df02d944f5baec8d92a14f
Marcel Baltzer <marcel.baltzer@hotmail.de>
2026-01-05 14:14:00 +0100

    delete first frontend WIP

--- src/app/lib/config/app_config.dart
+++
@@ -1,7 +0,0 @@
-class AppConfig {
-  static const cognitoRegion = String.fromEnvironment('COGNITO_REGION');
-
-  static const cognitoClientId = String.fromEnvironment('COGNITO_CLIENT_ID');
-
-  static const apiBaseUrl = String.fromEnvironment('API_BASE_URL');
-}
